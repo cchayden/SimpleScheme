528205187357a4010fe50b981d761c7dcc27825c
Charles Hayden <cchayden@gmail.com>
2011-06-22 21:23:25 -0700

    4-18c

--- SimpleScheme/Eval.cs
+++ SimpleScheme/Eval.cs
@@ -163,6 +163,9 @@ namespace SimpleScheme
     /// </summary>
     public class EvaluatorMain : Evaluator
     {
+        private object fn;
+        private object args;
+
         /// <summary>
         /// Initializes a new instance of the EvaluatorMain class.
         /// </summary>
@@ -186,122 +189,130 @@ namespace SimpleScheme
         /// <returns>The next step to execute.</returns>
         public override Evaluator EvalStep()
         {
-            // pick up results of call
-            if (this.Called != null)
+            // pick up results of call if returning or on first step
+            if (this.Called != null && Pc < 1)
             {
                 this.Expr = this.Called.RetExpr;
                 this.Env = this.Called.RetEnv;
             }
 
-            if (this.Pc == PcReturn)
+            switch (this.Pc)
             {
-                this.RetExpr = this.Expr;
-                return EvalReturn();
-            }
-
-            if (this.Expr is string)
-            {
-                // Evaluate a string by looking it up in the environment.
-                // It should correspond to a varialbe name, for which there 
-                //    is a corresponding value.
-                this.RetExpr = this.Env.Lookup((string)this.Expr);
-                return EvalReturn();
-            }
-
-            if (!(this.Expr is Pair))
-            {
-                // If we are evaluating something that is not a pair, 
-                //    it must be a constant.
-                // Return the integer, real, boolean, or vector.
-                this.RetExpr = this.Expr;
-                return EvalReturn();
-            }
-
-            // We are evaluating a pair.
-            // Split out the first item for special treatment.
-            object fn = First(this.Expr);
-            object args = Rest(this.Expr);
-
-            // Look for one of the special forms. 
-            switch (fn as string)
-            {
-                case "quote":
-                    // Evaluate quoted expression by just returning the expression.
-                    this.RetExpr = First(args);
+                case PcReturn:
+                    this.RetExpr = this.Expr;
                     return EvalReturn();
 
-                case "begin":
+                case 0:
+                    if (this.Expr is string)
                     {
-                        // Evaluate begin by evaluating all the items in order, 
-                        //   and returning the last.
-                        return SubEval(new EvaluatorSequence(this.Interp, this, args, this.Env));
+                        // Evaluate a string by looking it up in the environment.
+                        // It should correspond to a varialbe name, for which there 
+                        //    is a corresponding value.
+                        this.RetExpr = this.Env.Lookup((string) this.Expr);
+                        return EvalReturn();
                     }
 
-                case "define":
+                    if (!(this.Expr is Pair))
                     {
-                        // Define is a shortcut for lambda.
-                        // Evaluate by splicing lambda on the front and evaluating that.
-                        return SubEvalReturn(new EvaluatorDefine(this.Interp, this, args, this.Env));
+                        // If we are evaluating something that is not a pair, 
+                        //    it must be a constant.
+                        // Return the integer, real, boolean, or vector.
+                        this.RetExpr = this.Expr;
+                        return EvalReturn();
                     }
 
-                case "set!":
+                    // We are evaluating a pair.
+                    // Split out the first item for special treatment.
+                    fn = First(this.Expr);
+                    args = Rest(this.Expr);
+
+                    // Look for one of the special forms. 
+                    switch (fn as string)
                     {
-                        // Evaluate a set! expression by evaluating the second, 
-                        //   then setting the first to it.
-                        return SubEvalReturn(new EvaluatorSet(this.Interp, this, args, this.Env));
+                        case "quote":
+                            // Evaluate quoted expression by just returning the expression.
+                            this.RetExpr = First(args);
+                            return EvalReturn();
+
+                        case "begin":
+                            {
+                                // Evaluate begin by evaluating all the items in order, 
+                                //   and returning the last.
+                                return SubEval(new EvaluatorSequence(this.Interp, this, args, this.Env));
+                            }
+
+                        case "define":
+                            {
+                                // Define is a shortcut for lambda.
+                                // Evaluate by splicing lambda on the front and evaluating that.
+                                return SubEvalReturn(new EvaluatorDefine(this.Interp, this, args, this.Env));
+                            }
+
+                        case "set!":
+                            {
+                                // Evaluate a set! expression by evaluating the second, 
+                                //   then setting the first to it.
+                                return SubEvalReturn(new EvaluatorSet(this.Interp, this, args, this.Env));
+                            }
+
+                        case "if":
+                            {
+                                // Eval an if expression by evaluating the first clause, 
+                                //    and then returning either the second or third.
+                                return SubEval(new EvaluatorIf(this.Interp, this, args, this.Env));
+                            }
+
+                        case "cond":
+                            return SubEval(new EvaluatorReduceCond(this.Interp, this, args, this.Env));
+
+                        case "lambda":
+                            // Evaluate a lambda by creating a closure.
+                            this.RetExpr = new Closure(First(args), Rest(args), this.Env);
+                            return EvalReturn();
+
+                        case "macro":
+                            // Evaluate a macro by creating a macro.
+                            this.RetExpr = new Macro(First(args), Rest(args), this.Env);
+                            return EvalReturn();
                     }
 
-                case "if":
+                    // If we get here, it wasn't one of the special forms.  
+                    // So we need to evaluate the first item (the function) in preparation for
+                    //    doing a procedure call.
+                    Pc = 1;
+                    return CallEval(fn);
+
+               case 1:
+                    fn = Called.RetExpr;
+
+                    // If the function is a macro, expand it and then continue.
+                    if (fn is Macro)
                     {
-                        // Eval an if expression by evaluating the first clause, 
-                        //    and then returning either the second or third.
-                        return SubEval(new EvaluatorIf(this.Interp, this, args, this.Env));
+                        Macro m = (Macro) fn;
+                        this.Expr = m.Expand(this.Interp, (Pair) this.Expr, args);
+                        Pc = 0;
+                        return EvalContinue();
                     }
 
-                case "cond":
-                    return SubEval(new EvaluatorReduceCond(this.Interp, this, args, this.Env));
+                    // If the function is a closure, then create a new environment consisting of
+                    //   1 the closure param list
+                    //   2 arguments evaluated in the original environment
+                    //   3 the closure's environment
+                    // Then continue evaluating the closure body in this new environment
+                    if (fn is Closure)
+                    {
+                        // CLOSURE CALL -- capture the environment and continue with the body
+                        Closure f = (Closure) fn;
+                        SubEvalReturn(new EvaluatorClosure(Interp, this, args, Env, f));
+                    }
 
-                case "lambda":
-                    // Evaluate a lambda by creating a closure.
-                    this.RetExpr = new Closure(First(args), Rest(args), this.Env);
-                    return EvalReturn();
-
-                case "macro":
-                    // Evaluate a macro by creating a macro.
-                    this.RetExpr = new Macro(First(args), Rest(args), this.Env);
-                    return EvalReturn();
+                    // This is a procedure call.
+                    // In any other case, the function is a primitive or a user-defined function.
+                    // Evaluate the arguments in the environment, then apply the function 
+                    //    to the arguments.
+                    return SubEvalReturn(new EvaluatorApplyProc(Interp, this, args, Env, fn));
             }
-
-            // If we get here, it wasn't one of the special forms.  
-            // So we need to evaluate the first item (the function) in preparation for
-            //    doing a procedure call.
-            fn = this.Interp.Eval(fn, this.Env);      // TODO
-
-            // If the function is a macro, expand it and then continue.
-            if (fn is Macro)
-            {
-                Macro m = (Macro)fn;
-                this.Expr = m.Expand(this.Interp, (Pair) this.Expr, args);
-                return EvalContinue();
-            }
-
-            // If the function is a closure, then create a new environment consisting of
-            //   1 the closure param list
-            //   2 arguments evaluated in the original environment
-            //   3 the closure's environment
-            // Then continue evaluating the closure body in this new environment
-            if (fn is Closure)
-            {
-                // CLOSURE CALL -- capture the environment and continue with the body
-                Closure f = (Closure)fn;
-                SubEvalReturn(new EvaluatorClosure(Interp, this, args, Env, f));
-            }
-
-            // This is a procedure call.
-            // In any other case, the function is a primitive or a user-defined function.
-            // Evaluate the arguments in the environment, then apply the function 
-            //    to the arguments.
-            return SubEvalReturn(new EvaluatorApplyProc(Interp, this, args, Env, fn));
+            throw new Exception("EvaluatorMain pc error");
         }
     }
 
