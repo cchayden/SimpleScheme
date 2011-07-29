5dfebfbd11e1cea0afbb3d917f491bbc9528edd9
Charles Hayden <cchayden@gmail.com>
2011-07-29 09:23:34 -0700

    Add simplest example app.

---
+++ Examples/BareInterpreter/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// This is the simplest example of an application embedding the Simple Scheme interpreter.
+namespace BareInterpreter
+{
+    using SimpleScheme;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Interpreter().ReadEvalPrintLoop();
+        }
+    }
+}
