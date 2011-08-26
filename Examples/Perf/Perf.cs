4d5f7f38165dad5be7c99e9001074b3782b5c247
Charles Hayden <cchayden@gmail.com>
2011-08-25 20:18:53 -0700

    Add perf example.

---
+++ Examples/Perf/Perf.cs
@@ -0,0 +1,11 @@
+﻿// Measures the performance of the interpreter
+using System;
+using SimpleScheme;
+class Perf
+{
+    static void Main()
+    {
+        Interpreter.New(new[] { "perf.ss" });
+        Console.ReadLine();
+    }
+}
